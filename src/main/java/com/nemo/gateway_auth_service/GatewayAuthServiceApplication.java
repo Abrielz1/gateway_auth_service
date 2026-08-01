145c54ce9a954056c797de1dee56157702886238
Abrielz1 <abrielz11@gmail.com>
2026-08-02 02:02:11 +0300

    curing k8s fuckup

--- src/main/java/com/nemo/gateway_auth_service/GatewayAuthServiceApplication.java
+++ src/main/java/com/nemo/gateway_auth_service/GatewayAuthServiceApplication.java
@@ -2,10 +2,8 @@ package com.nemo.gateway_auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
-@ConfigurationPropertiesScan
+@SpringBootApplication(scanBasePackages = "com.nemo.gateway_auth_service")
 public class GatewayAuthServiceApplication {
 
 	public static void main(String[] args) {
